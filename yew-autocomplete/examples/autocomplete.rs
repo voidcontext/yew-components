73df7cd83f47f2052c40bfae4576215711d2990a
Gabor Pihaj <gabor.pihaj@gmail.com>
2023-01-18 07:51:56 +0000

    Add incomplete AutoComplete component to the example

--- yew-autocomplete/examples/autocomplete.rs
+++ yew-autocomplete/examples/autocomplete.rs
@@ -1,11 +1,17 @@
+#[rustfmt::skip::macros(html)]
 use yew::prelude::*;
+
+use yew_autocomplete::yew_autocomplete::AutoComplete;
 
 use wasm_bindgen::prelude::*;
 
 #[function_component(App)]
 fn app() -> Html {
     html! {
-        <h1>{"yew-commons: Autocomplete Demo"}</h1>
+        <>
+            <h1>{"yew-commons: Autocomplete Demo"}</h1>
+            <AutoComplete />
+        </>
     }
 }
 
