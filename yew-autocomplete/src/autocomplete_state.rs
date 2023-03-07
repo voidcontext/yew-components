387d23a0a5eb128b3c8598ef243b73cd7e0be710
Gabor Pihaj <gabor.pihaj@gmail.com>
2023-03-07 21:51:14 +0000

    Introduce min length for completion + clear items when length less then min

--- yew-autocomplete/src/autocomplete_state.rs
+++ yew-autocomplete/src/autocomplete_state.rs
@@ -33,13 +33,18 @@ impl<T: Clone + 'static> AutocompleteState<T> {
     ) {
         self.input = value.to_string();
 
-        let string = value.to_string();
+        let string = self.input.clone();
 
-        dispatcher(Box::pin(async move {
-            let items = (ir.fun)(string).await;
+        // TODO: make the min length configurable
+        if string.len() > 2 {
+            dispatcher(Box::pin(async move {
+                let items = (ir.fun)(string).await;
 
-            Msg::SetItems(items.unwrap())
-        }));
+                Msg::SetItems(items.unwrap())
+            }));
+        } else {
+            dispatcher(Box::pin(async move { Msg::SetItems(vec![]) }));
+        }
     }
 
     pub fn set_items(&mut self, items: Vec<T>) {
@@ -49,6 +54,8 @@ impl<T: Clone + 'static> AutocompleteState<T> {
 
 #[cfg(test)]
 mod tests {
+    use core::panic;
+
     use crate::{ItemResolverResult, Msg};
 
     use super::AutocompleteState;
@@ -72,6 +79,8 @@ mod tests {
         assert_eq!(state.input, "this is a text");
     }
 
+    // --- oninput
+
     #[wasm_bindgen_test]
     async fn test_oninput_should_resolve_autocomplete_items() {
         let mut state = AutocompleteState::default();
@@ -94,6 +103,46 @@ mod tests {
         let sent = rx.await.unwrap();
         assert_eq!(sent, Msg::SetItems(vec!["result".to_string()]));
     }
+
+    #[wasm_bindgen_test]
+    fn test_oninput_should_not_resolve_autocomplete_items_when_input_is_short() {
+        let mut state = AutocompleteState::default();
+
+        state.oninput(
+            "th",
+            |_f| {},
+            FnProp::from(|_s: String| -> ItemResolverResult<String> {
+                panic!("Shouldn't be called");
+            }),
+        );
+
+        assert_eq!(state.input(), "th".to_string());
+    }
+
+    #[wasm_bindgen_test]
+    async fn test_oninput_should_clear_items_when_input_is_short() {
+        let mut state = AutocompleteState::default();
+
+        let (tx, rx) = futures::channel::oneshot::channel::<Msg<String>>();
+
+        state.oninput(
+            "th",
+            |f| {
+                spawn_local(async move {
+                    let msg = f.await;
+                    tx.send(msg).unwrap();
+                });
+            },
+            FnProp::from(|_s: String| -> ItemResolverResult<String> {
+                panic!("Shouldn't be called")
+            }),
+        );
+
+        let sent = rx.await.unwrap();
+        assert_eq!(sent, Msg::SetItems(vec![]));
+    }
+
+    // --- oninput
 
     #[wasm_bindgen_test]
     fn test_set_items_sets_the_items() {
